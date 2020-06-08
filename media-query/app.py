e103f2b95018f3898e107313c1214af74445013a
hansol-lim <gksthf070@gmail.com>
2020-06-08 17:12:19 +0900

    add lambda function

--- media-query/app.py
+++ media-query/app.py
@@ -1,29 +1,22 @@
+import boto3
 from chalice import Chalice
+from chalicelib import rekognition
 
 app = Chalice(app_name='media-query')
 
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+_REKOGNITION_CLIENT = None
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+def get_rekognition_client():
+    global _REKOGNITION_CLIENT
+    if _REKOGNITION_CLIENT is None:
+        _REKOGNITION_CLIENT = rekognition.RekognitonClient(
+            boto3.client('rekognition'))
+    return _REKOGNITION_CLIENT
+
+
+@app.lambda_function()
+def detect_labels_on_image(event, context):
+    bucket = event['Bucket']
+    key = event['Key']
+    return get_rekognition_client().get_image_labels(bucket=bucket, key=key)
